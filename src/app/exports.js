418b4370cb348108f67ac1cf3c51acd4784db2ba
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2019-11-13 17:55:13 -0300

    exports.js default

--- src/app/exports.js
+++ src/app/exports.js
@@ -1,7 +1,3 @@
-import Dashboard from './dashboard/dashboard'
-import User from '../default/users/index'
-import Profile from '../default/profiles/index'
-
 // Reducers do projeto
 export const reducers = {
 
@@ -9,20 +5,10 @@ export const reducers = {
 
 // Rotas do projeto
 export const routes = [
-    { exact: true, path: '/', component: Dashboard },
-    { exact: true, path: '/users', component: User },
-    { exact: true, path: '/profiles', component: Profile }
+
 ]
 
 // Menu do projeto
 export const menu = {
-    '/': { title: 'Dashboard', icon: 'dashboard' },
-    '/users': { title: 'Usuários', icon: 'user' },
-    'profiles': {
-        title: 'Perfis', icon: 'users',
-        //Exemplo de menu cascateado
-        // children: {
-        //     '/permissions': { title: 'Permissões', icon: 'user' },
-        // }
-    }
+
 }
