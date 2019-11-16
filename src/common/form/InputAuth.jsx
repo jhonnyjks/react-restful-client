227f81a6387b9c0e5f0cfcbbc4625f411c82594a
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2019-11-16 10:25:53 -0300

    Refatorando tela de Login

--- src/common/form/InputAuth.jsx
+++ src/common/form/InputAuth.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import If from '../operator/If'
 export default props => (
     <If test={!props.hide}>
-        <div className="form-group has-feedback">
+        <div className="input-group mb-3 has-feedback">
             <input {...props.input}
                 className='form-control'
                 placeholder={props.placeholder}
                 readOnly={props.readOnly}
                 type={props.type} />
-            <span className={`glyphicon glyphicon-${props.icon}
-form-control-feedback`}></span>
+            <div class="input-group-append">
+                <div class="input-group-text">
+                    <span className={`fas fa-${props.icon}`}></span>
+                </div>
+            </div>
         </div>
     </If>
 )
