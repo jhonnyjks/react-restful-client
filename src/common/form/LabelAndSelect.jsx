ce09a655f8a66cfdb5828ad83f2dce7e3d7acbf8
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2019-12-07 12:40:35 -0300

    Criando componente LabelAndSelect

---
+++ src/common/form/LabelAndSelect.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Grid from '../layout/grid'
+
+export default props => (
+    <Grid cols={props.cols}>
+        <div className='form-group'>
+            <label htmlFor={props.name}>{props.label}</label>
+            <select name={props.name} {...props.input}
+                readOnly={props.readOnly} className="custom-select mb-3">
+                <option value="">{props.placeholder}</option>
+                {props.options && props.options.map(e => (<option value={e.id}>{e.name}</option>))}
+            </select>
+        </div>
+    </Grid>
+)
