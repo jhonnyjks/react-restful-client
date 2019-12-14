cda4ec74fdb8a0a2dbd75cfdf16d563446711363
Fredson Rodrigues <fredson.rodrigues.principal@gmail.com>
2019-12-14 12:55:23 -0300

    Preparando LabelAndSelect para exibir erros de validaçao

--- src/common/form/LabelAndSelect.jsx
+++ src/common/form/LabelAndSelect.jsx
@@ -1,15 +1,58 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Grid from '../layout/grid'
 
-export default props => (
-    <Grid cols={props.cols}>
-        <div className='form-group'>
-            <label htmlFor={props.name}>{props.label}</label>
-            <select name={props.name} {...props.input}
-                readOnly={props.readOnly} className="custom-select mb-3">
-                <option value="">{props.placeholder}</option>
-                {props.options && props.options.map(e => (<option key={e.id} value={e.id}>{e.name}</option>))}
-            </select>
-        </div>
-    </Grid>
-)
+class LabelAndSelect extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: {
+                message: null,
+                flag: false
+            }
+        }
+    }
+
+    componentWillUpdate(nextProps) {
+        if (nextProps.error) {      
+            if (this.state.error.flag === false) {
+                if (Object.keys(nextProps.error).length > 0 && nextProps.error[this.props.input.name]) {
+                    this.setState({
+                        error: {
+                            message: nextProps.error[this.props.input.name][0],
+                            flag: true
+                        }
+                    })
+                }
+            } else {
+                if (Object.keys(nextProps.error).length === 0 || nextProps.error[this.props.input.name] === undefined) {
+                    this.setState({
+                        error: {
+                            message: null,
+                            flag: false
+                        }
+                    })
+                }
+            }
+        }
+    }
+
+    render() {
+        return (
+            <Grid cols={this.props.cols}>
+                <div className='form-group'>
+                    <label htmlFor={this.props.name}>{this.props.label}</label>
+                    <select name={this.props.name} {...this.props.input}
+                        readOnly={this.props.readOnly} className={`custom-select mb-3 ${this.state.error.flag === true ? `is-invalid` : ``}`}>
+                        <option value="">{this.props.placeholder}</option>
+                        {this.props.options && this.props.options.map(e => (<option key={e.id} value={e.id}>{e.name}</option>))}
+                    </select>
+                    <div className="invalid-feedback">
+                        {this.state.error.flag === true ? this.state.error.message : "Campo inválido"}
+                    </div>
+                </div>
+            </Grid>
+        );
+    }
+}
+
+export default LabelAndSelect;
