2e16df1699ccca63fa51b61dd653870f0000cd12
Jhonny Mesquita <jhonnyjks@gmail.com>
2020-08-14 19:48:11 -0300

    Adicionado um padding na div de conteúdo principal

--- src/common/template/Content.jsx
+++ src/common/template/Content.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default props => (
-    <section className='content'>
+    <section className='content' style={{paddingBottom: '40px'}}>
         <div className="container-fluid">
             {props.children}
         </div>
