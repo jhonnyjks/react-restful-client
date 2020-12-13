9f8f565a8b77ac041007479a6706fa7da53083c6
Jhonny Mesquita <jhonnyjks@gmail.com>
2020-12-13 11:54:33 -0300

    Correções e melhorias: Possibilitado usar os componentes de formulário sem Redux.

--- src/common/layout/widget/Box.jsx
+++ src/common/layout/widget/Box.jsx
@@ -7,7 +7,7 @@ export default props => (
         <div className={`box box-warning material-item bg-${props.color}`} style={{ marginBottom: 0 }}>
 
             <div className='box-header with-border'>
-                <h3 className='box-title'>{props.title}</h3>
+                <h3 className='box-title' style={{display:'initial'}}>{props.title}</h3>
                 <div className='box-tools pull-right'>
                     <button type='button' className='btn btn-box-tool' data-widget='collapse'><i className='fa fa-minus'></i>
                     </button>
