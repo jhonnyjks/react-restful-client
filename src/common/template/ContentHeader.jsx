48c06130270ec7e51282c9b51e7145974c5166f4
Jhonny Mesquita <jhonnyjks@gmail.com>
2022-12-19 12:31:53 -0300

    Diminuir cabeçalho de todos os painéis.

--- src/common/template/ContentHeader.jsx
+++ src/common/template/ContentHeader.jsx
@@ -7,7 +7,7 @@ const history = createBrowserHistory();
 export default props => (
     <section className='content-header'>
         <div className="container-fluid">
-        <div className="row mb-2">
+        <div className="row">
             <div className="col-sm-6">
                 <h1 className="pull-left">{props.title} <small style={{fontSize: 15}}>{props.small}</small></h1>
             </div>
