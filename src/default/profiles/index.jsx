c4f0e4ff1f8da5420e27b0c2fa0683e58fab9525
Tatilon <tatilon.lino@gmail.com>
2023-10-25 13:04:37 -0300

    trocar-perfil reinsert

--- src/default/profiles/index.jsx
+++ src/default/profiles/index.jsx
@@ -9,12 +9,25 @@ import If from '../../common/operator/If'
 import List from './List'
 import Form from './Form'
 import { getList, showContent, update, init, create, showUpdate } from './actions'
+import Auth from '../auth/auth'
 
 class Profile extends Component {
+
+      //Necessário para gerar o contexto em this.context
+      static contextTypes = {
+        router: () => null, // replace with PropTypes.object if you use them
+    }
+
+
+
+
+  
+    
 
     componentWillMount() {
         this.props.init()
         this.props.getList()
+        
     }
 
     render() {
@@ -30,17 +43,24 @@ class Profile extends Component {
 
         return (
             <div>
+               
+               <If test={this.context.router.route.location.pathname != "/trocar-perfil"}>
                 <ContentHeader title='Perfis' small='Gerenciar perfis de usuário'
-                    createMethod={() => this.props.showContent('form')} />
-                <Content>
-                    <If test={this.props.show === 'list'}>
-                        <List />
-                    </If>
-                    <If test={this.props.show === 'form'}>
-                        <Form onSubmit={this.props.isEdit ? this.props.update : this.props.create}
-                            submitLabel='Salvar' submitClass='primary' />
-                    </If>
-                </Content>
+                        createMethod={() => this.props.showContent('form')} />
+                    <Content>
+                        <If test={this.props.show === 'list'}>
+                            <List />
+                        </If>
+                        <If test={this.props.show === 'form'}>
+                            <Form onSubmit={this.props.isEdit ? this.props.update : this.props.create}
+                                submitLabel='Salvar' submitClass='primary' />
+                        </If>
+                    </Content>
+               </If>
+                
+                <If test={this.context.router.route.location.pathname == "/trocar-perfil"}>
+                        <Auth></Auth>                
+                </If>    
             </div>
         )
     }
