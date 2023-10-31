61f6f2757fc4f9cfb81581cbb6f6dafebdca7169
matheus1002 <matheus10carneiro@gmail.com>
2023-10-30 23:14:08 -0300

    feat: entity check in global can method

--- src/common/helpers/index.js
+++ src/common/helpers/index.js
@@ -9,10 +9,22 @@ const codesPerOperation = {
     8: [8, 9, 10, 11, 12, 13, 14, 15] // 8-Delete
 }
 
-export function can(scopes, entity, scope, operation) {
+function camelToSnake(text) {
+    return text.replace(/([A-Z])/g, '_$1').replace(/^_/, '').toLowerCase();
+}
+
+export function can(reduxState, entity, scope, operation, id) {
+
+    let entityName = camelToSnake(entity);
+
+    const scopes = reduxState.auth.profile.scopes
+
+    const list = reduxState[entityName].list && reduxState[entityName].list.length > 0
+        ? reduxState[entityName].list
+        : reduxState[entityName].list_update;
+
     // Percorra os objetos de escopo
     for (const key in scopes) {
-        console.log(key)
         const scopesObject = scopes[key].scopes;
 
         // Verifique se o 'entity' corresponde ao valor passado como argumento
@@ -30,5 +42,32 @@ export function can(scopes, entity, scope, operation) {
         }
     }
 
+    // Percorra os objetos de list
+    for (const key in list) {
+
+        const itemObject = list[key]
+        const itemId = list[key].id
+
+        // Verifique se itemId é igual a id
+        if (itemId === id) {
+            
+            let permissions = itemObject.process_permissions;
+
+            // Percorra os objetos de permissions
+            permissions.forEach(permission => {
+
+                // Verifique se o escopo desejado existe no item
+                if (permission.scope === scope) {
+                    const permissionCode = permission.code;
+
+                    // Verifique se o código de operação está presente no array de códigos de operação
+                    if (codesPerOperation[operation].indexOf(permissionCode) > -1) {
+                        return true; // Código de operação válido, retorne true
+                    }
+                }
+            });
+        }
+    }
+    
     return false;
 }
