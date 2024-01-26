9bad5a65b4b5c429f52cc69d07f9c17d0fef54ac
Tatilon <tatilon.lino@gmail.com>
2024-01-26 20:58:51 -0300

    merge component LabelAndSelect isMulti

--- src/common/form/LabelAndSelect.jsx
+++ src/common/form/LabelAndSelect.jsx
@@ -51,11 +51,25 @@ class LabelAndSelect extends Component {
         }
     }
 
+    // handleSelectChange = (selectedOptions) => {
+    //     // Supondo que `input.onChange` espera um array de valores
+    //     const values = selectedOptions.map(option => option.value);
+    //     this.props.input.onChange(values);
+    // }
     handleSelectChange = (selectedOptions) => {
-        // Supondo que `input.onChange` espera um array de valores
-        const values = selectedOptions.map(option => option.value);
-        this.props.input.onChange(values);
+        if (Array.isArray(selectedOptions)) {
+            // Para múltiplas seleções, extrai os valores dos objetos selecionados
+            const values = selectedOptions.map(option => option.value);
+            this.props.input.onChange(values);
+        } else if (selectedOptions) {
+            // Para uma única seleção, extrai o valor do objeto selecionado
+            this.props.input.onChange(selectedOptions.value);
+        } else {
+            // Caso nenhuma opção seja selecionada
+            this.props.input.onChange(null);
+        }
     }
+    
 
     componentWillUpdate(nextProps) {
         if (nextProps.error) {      
@@ -101,94 +115,68 @@ class LabelAndSelect extends Component {
         return rules
             
     }
-
+  
     render() {
-        const formName = this.props.meta ? this.props.meta.form : null
+        const formName = this.props.meta ? this.props.meta.form : null;
         const isMulti = this.props.isMulti || false;
-        let scope = ''
-        let permission = 0
-        let rules = []
-        let action = null
+        let scope = '';
+        let permission = 0;
+        let rules = [];
+        let action = null;
 
-        if(formName) {
-            scope = _.findKey(this.props.scopes, ['entity', _.upperFirst(formName.split('Form')[0])])
-            permission = this.props.scopes[scope] ? this.props.scopes[scope].actions[this.props.input.name] || 0 : 0
-            rules = this.getValidation(scope)
-            action = this.props.forms[formName] && this.props.forms[formName].values && this.props.forms[formName].values.id ? 'update' : 'insert'
+        if (formName) {
+            scope = _.findKey(this.props.scopes, ['entity', _.upperFirst(formName.split('Form')[0])]);
+            permission = this.props.scopes[scope] ? this.props.scopes[scope].actions[this.props.input.name] || 0 : 0;
+            rules = this.getValidation(scope);
+            action = this.props.forms[formName] && this.props.forms[formName].values && this.props.forms[formName].values.id ? 'update' : 'insert';
         }
 
-        // console.log(this.props.input.name, this.props.label);
-
-        // if (this.state.error.flag === true) {
-        //     console.log(this.state.error.message.replace(this.props.input.name, this.props.label));
-
-        console.log(this.state.error.message);
-
-        //     console.log(this.state.error.message.replace(
-        //         new RegExp(this.props.input.name.replace(/_/g, ' '), 'i'),
-        //         this.props.label
-        //     ));
-            
-        // }
-
-        const { input, options } = this.props;
-        const selectedValues = Array.isArray(input.value) ? input.value : [input.value];
-        const selectedOptions = options.filter(option =>
-            selectedValues.includes(option.id || option.value)
+        const selectedValues = Array.isArray(this.props.input.value) ? this.props.input.value : [this.props.input.value];
+       
+        // const selectedOptions = this.props.options.filter(option =>
+        //     selectedValues.includes(option.id || option.value)
+        // ).map(option => ({
+        //     value: option.id || option.value,
+        //     label: option[this.props.textAttr] || option.name || option.noun || option.title || option.description || option.id
+        // }));
+        const selectedOptions = this.props.options.filter(option =>
+            Array.isArray(this.props.input.value) 
+                ? this.props.input.value.includes(option.id || option.value)
+                : this.props.input.value === (option.id || option.value)
         ).map(option => ({
             value: option.id || option.value,
             label: option[this.props.textAttr] || option.name || option.noun || option.title || option.description || option.id
         }));
 
-        
+        const selectStyles = isMulti ? {} : { menu: base => ({ ...base, zIndex: 9999 }) };
+        const selectClassName = `basic-single-select ${this.state.error.flag === true ? `is-invalid` : ``}`;
 
         return (
             <>
-            { (this.props.readOnly === false || this.hasPermission(permission, 'read') || this.props.forceToShow) && <Grid cols={this.props.cols} style={this.props.style || {}} className={this.props.className || {}} >
-                <div className='form-group'>
-                    <label htmlFor={this.props.name}>{this.props.label}</label>
+                {(this.props.readOnly === false || this.hasPermission(permission, 'read') || this.props.forceToShow) && <Grid cols={this.props.cols} style={this.props.style || {}} className={this.props.className || {}}>
+                    <div className='form-group'>
+                        <label htmlFor={this.props.name}>{this.props.label}</label>
 
-                    {isMulti ? (
                         <Select
-                            isMulti
-                            name={this.props.name}
-
+                            isMulti={isMulti}
+                            name={this.props.input.name}
                             options={this.props.options.map(option => ({
                                 value: option.id || option.value,
                                 label: option[this.props.textAttr] || option.name || option.noun || option.title || option.description || option.id
                             }))}
-                            className={`basic-multi-select ${this.state.error.flag === true ? `is-invalid` : ``}`}
+                            className={selectClassName}
                             classNamePrefix="select"
+                            styles={selectStyles}
                             required={rules['required'] || false}
                             onChange={this.handleSelectChange}
-                            value={selectedOptions}
+                            value={isMulti ? selectedOptions : selectedOptions[0]} // Ajuste aqui para suportar seleção única
                         />
-                    ) : (
-                        <select name={this.props.name} {...this.props.input}
-                            disabled={this.props.readOnly !== false ? this.props.readOnly || !this.hasPermission(permission, action) : false}
-                            className={`custom-select mb-3 ${this.state.error.flag === true ? `is-invalid` : ``}`}
-                            required={rules['required'] || false} >
-                            <option value="">{this.props.placeholder}</option>
-                            {this.props.options && this.props.options[0] && this.props.options.map(
-                                e => (((e && e.id > -1) || (e && e.value)) ? (<option key={e.id || e.value} value={e.id || e.value}>
-                                        { (this.props.callback ? this.props.callback(e) : null) || e[this.props.textAttr] || e.name || e.noun || e.title || e.description || e.id }
-                                    </option>) : <></>)
-                            )}
-                        </select>
-                    )}
-                    <div className="invalid-feedback">
-                        {
-                            this.state.error.flag === true ?
-                            (
-                                this.props.input.name.includes('_') ?
-                                this.state.error.message.replace(new RegExp(this.props.input.name.replace(/_/g, ' '), 'i'), this.props.label) :
-                                this.state.error.message.replace(this.props.input.name, this.props.label)
-                            ) :
-                            "Valor inválido informado"
-                        }
+
+                        <div className="invalid-feedback">
+                            {this.state.error.flag && (this.state.error.message.replace(this.props.input.name, this.props.label))}
+                        </div>
                     </div>
-                </div>
-            </Grid>}
+                </Grid>}
             </>
         )
     }
