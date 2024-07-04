4877b93c067d95f67e2ab601d0e02630a85ad66b
matheus1002 <matheus10carneiro@gmail.com>
2024-07-04 17:21:02 -0300

    feat: melhoria botoes menu

--- src/common/template/ProfileHeader.jsx
+++ src/common/template/ProfileHeader.jsx
@@ -1,87 +1,111 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { Link } from 'react-router-dom'
-
-import { logout } from '../../default/auth/authActions'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../../default/auth/authActions';
 
 class ProfileHeader extends Component {
     constructor(props) {
-        super(props)
-        this.state = { open: false }
+        super(props);
+        this.state = { open: false };
+        this.changeOpen = this.changeOpen.bind(this);
     }
+
     changeOpen(e) {
-        e.preventDefault()
-        this.setState({ open: !this.state.open })
+        e.preventDefault();
+        this.setState({ open: !this.state.open });
     }
 
     RenderSidebarMenu = () => {
+
+        const { hover } = this.props;
+
         return (
             <React.Fragment>
                 <div className="mt-2">
-                    <ul className='nav nav-pills nav-sidebar flex-column'>
-                        <li className="nav-item">
-                            <div href="#!" className="nav-link">
-                                <i className="nav-icon fas fa-power-off"></i>
-                                <span> Deseja Sair?</span>
-                            </div>
-                        </li>
-                        <li className="nav-item mb-1">
-                            <a href="#!" className="nav-link bg-success" onClick={this.props.logout}>
+
+                    {this.state.open && hover && (
+                        <div className="mt-2">
+                            <ul className='nav nav-pills nav-sidebar flex-column'>
+                                <li className="nav-item">
+                                    <div href="#!" className="nav-link">
+                                        <i className="nav-icon fas fa-power-off"></i>
+                                        <span> Deseja Sair?</span>
+                                    </div>
+                                </li>
+                                <li className="nav-item mb-1">
+                                    <a href="#!" className="nav-link bg-success" onClick={this.props.logout}>
+                                        <i className="nav-icon fas fa-check"></i>
+                                        <span> Sim</span>
+                                    </a>
+                                </li>
+                                <li className="nav-item mb-1">
+                                    <a href="#!" className="nav-link bg-danger" onClick={this.changeOpen}>
+                                        <i className="nav-icon fas fa-times"></i>
+                                        <span> Não</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    )}
+
+                    {!hover && (
+                        <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <a href="#!" className="d-block mt-2 btn btn-secondary btn-sm">
+                                <i className="nav-icon fas fa-question"></i>
+                            </a>
+                            <a href="#!" className="d-block mt-2 btn btn-success btn-sm">
                                 <i className="nav-icon fas fa-check"></i>
-                                <span> Sim</span>
                             </a>
-                        </li>
-                        <li className="nav-item mb-1">
-                            <a href="#!" className="nav-link bg-danger" onClick={(e) => this.changeOpen(e)}>
+                            <a href="#!" className="d-block mt-2 btn btn-danger btn-sm">
                                 <i className="nav-icon fas fa-times"></i>
-                                <span> Não</span>
                             </a>
-                        </li>
-                    </ul>
+                        </div>
+                    )}
                 </div>
                 <div className="user-panel mt-1 pb-1 mb-1 d-flex"></div>
             </React.Fragment>
-        )
+        );
     }
 
     render() {
-        const { name, /*email*/ } = this.props.user
-        const { RenderSidebarMenu } = this
+        const { name, /*email*/ } = this.props.user;
+        const { hover } = this.props;
+
         return (
             <React.Fragment>
-
-                <div>
-                    <div style={{ display: 'flex', flexDirection: 'column' }} >
-                        {
-                            this.props.profiles.length > 1 && <Link to="/trocar-perfil" className="d-block mt-2 btn btn-secondary btn-sm custom-bg-color" style={{ color: '' }}>
-                                <i className="nav-icon fas fa-sync-alt" style={{ width: '16.67px', height: '16.67px', top: '1.67px', left: '1.67px' }}></i>
-                                <span style={{ fontWeight: '400', fontSize: '16px', lineHeight: '24px', paddingLeft: '10px' }}>Trocar Perfil</span>
-                            </Link>
-                        }
-                        <a href="#!" className="d-block mt-2 btn btn-secondary btn-sm custom-bg-color" onClick={(e) => this.changeOpen(e)}>
-                            <i className="nav-icon fas fa-power-off"></i>
-                            <span> Sair</span>
-                        </a>
-                    </div>
+                <div style={{ display: 'flex', flexDirection: 'column' }}>
+                    {this.props.profiles.length > 1 && (
+                        <Link to="/trocar-perfil" className="d-block mt-2 btn btn-secondary btn-sm custom-bg-color" style={{ color: '' }}>
+                            <i className="nav-icon fas fa-sync-alt"></i>
+                            {hover && <span style={{ fontWeight: '400', fontSize: '16px', lineHeight: '24px', paddingLeft: '10px' }}>Trocar Perfil</span>}
+                        </Link>
+                    )}
+                    <a href="#!" className="d-block mt-2 btn btn-secondary btn-sm custom-bg-color" onClick={(e) => this.changeOpen(e)}>
+                        <i className="nav-icon fas fa-power-off"></i>
+                        {hover && <span> Sair</span>}
+                    </a>
                 </div>
 
-                {this.state.open && (<RenderSidebarMenu />)}
+                {this.state.open && (<this.RenderSidebarMenu />)}
 
-                <div className="user-panel mt-3 pb-3 mb-1 d-flex align-items-center" style={{ borderBottom: 'none' }}>
-                    <div className="image">
+                <div className={`user-panel mt-3 pb-3 mb-1 d-flex align-items-center ${hover ? '' : 'justify-content-center'}`} style={{ borderBottom: 'none' }}>
+                    <div className={`image ${hover ? '' : 'text-center'}`}>
                         <img src="https://dummyimage.com/160x160/fff/ggg" className="img-circle elevation-2" alt="User" />
                     </div>
-                    <div className="info pl-3">
-                        <a href="#!" className="d-block text-white font-weight-bold">{name}</a>
-                        <a href="#!" className="d-block text-white">{this.props.profile.noun}</a>
-                    </div>
+                    {hover && (
+                        <div className="info pl-3">
+                            <a href="#!" className="d-block text-white font-weight-bold">{name}</a>
+                            <a href="#!" className="d-block text-white">{this.props.profile.noun}</a>
+                        </div>
+                    )}
                 </div>
 
             </React.Fragment>
-        )
+        );
     }
 }
-const mapStateToProps = state => ({ user: state.auth.user, profile: state.auth.profile, profiles: state.auth.profiles })
-const mapDispatchToProps = dispatch => bindActionCreators({ logout }, dispatch)
-export default connect(mapStateToProps, mapDispatchToProps)(ProfileHeader)
+
+const mapStateToProps = state => ({ user: state.auth.user, profile: state.auth.profile, profiles: state.auth.profiles });
+const mapDispatchToProps = dispatch => bindActionCreators({ logout }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileHeader);
--- src/common/template/SideBar.jsx
+++ src/common/template/SideBar.jsx
@@ -1,76 +1,50 @@
-import React from "react";
-import Sidebar from "react-sidebar";
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { openCloseMiniSideBar, setSideBar } from './templateActions'
-import Menu from './Menu'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { openCloseMiniSideBar, setSideBar } from './templateActions';
+import Menu from './Menu';
 import ProfileHeader from './ProfileHeader';
 
-const SidebarSema = props => (
-    <aside className='main-sidebar sidebar-dark-success elevation-4' style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <a href="#!" className="brand-link">
-            { process.env.REACT_APP_LOGO ?
-            <img src={process.env.REACT_APP_LOGO} alt={process.env.REACT_APP_NAME} className="brand-image brand-image--custom"></img>
-            : <span className="brand-text font-weight-light text-center"><strong>{process.env.REACT_APP_NAME}</strong></span>
-            }
-        </a>
-        <div className='sidebar sidebar--sema' style={{ flex: 2, overflowY: 'auto'}}>
-            <Menu />
-        </div>
-
-        <div style={{ paddingRight: '20px', paddingLeft: '20px', marginBottom: '-40px' }}>
-            <ProfileHeader/>
-        </div>
-    </aside>
-)
-
-const mql = window.matchMedia(`(min-width: 800px)`);
-class SideBar extends React.Component {
+class SidebarSema extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            sidebarDocked: mql.matches,
-            sidebarOpen: false
+            hover: false
         };
-
-        this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
     }
 
-    componentWillMount() {
-        this.setState({ sidebarOpen: this.props.template.sideBarOpened });
-        mql.addListener(this.mediaQueryChanged);
+    handleMouseEnter = () => {
+        this.setState({ hover: true });
     }
 
-    componentWillUnmount() {
-        mql.removeListener(this.mediaQueryChanged);
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({ sidebarOpen: nextProps.template.sideBarOpened });
-    }
-
-    mediaQueryChanged() {
-        this.props.setSideBar(false)
-        this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
+    handleMouseLeave = () => {
+        this.setState({ hover: false });
     }
 
     render() {
         return (
-            <Sidebar
-                sidebar={<b>Sidebar content</b>}
-                open={this.state.sidebarOpen}
-                docked={this.state.sidebarDocked}
-                onSetOpen={(e) => this.props.openCloseMiniSideBar(e)}
-                shadow={false}
-                styles={{ root: { position: "relative" },  sidebar: { background: "none" } }}
-            >
-                <SidebarSema />
-            </Sidebar>
+            <aside className='main-sidebar sidebar-dark-success elevation-4'
+                   style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
+                   onMouseEnter={this.handleMouseEnter}
+                   onMouseLeave={this.handleMouseLeave}>
+                <a href="#!" className="brand-link">
+                    { process.env.REACT_APP_LOGO ?
+                    <img src={process.env.REACT_APP_LOGO} alt={process.env.REACT_APP_NAME} className="brand-image brand-image--custom"></img>
+                    : <span className="brand-text font-weight-light text-center"><strong>{process.env.REACT_APP_NAME}</strong></span>
+                    }
+                </a>
+                <div className='sidebar sidebar--sema' style={{ flex: 2, overflowY: 'auto'}}>
+                    <Menu />
+                </div>
+
+                <div style={{ paddingRight: '20px', paddingLeft: '20px', marginBottom: '-40px' }}>
+                    <ProfileHeader hover={this.state.hover} />
+                </div>
+            </aside>
         );
     }
-
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ openCloseMiniSideBar, setSideBar }, dispatch)
-const mapStateToProps = state => ({ template: state.template })
-export default connect(mapStateToProps, mapDispatchToProps)(SideBar);
+const mapDispatchToProps = dispatch => bindActionCreators({ openCloseMiniSideBar, setSideBar }, dispatch);
+const mapStateToProps = state => ({ template: state.template });
+export default connect(mapStateToProps, mapDispatchToProps)(SidebarSema);
