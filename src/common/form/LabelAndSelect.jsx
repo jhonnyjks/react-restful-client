b7459001258e01b2ae3da6ec9d0550d0faf37f84
matheus1002 <matheus10carneiro@gmail.com>
2024-08-19 14:26:22 -0300

    feat: alteracao no LabelAndSelect

--- src/common/form/LabelAndSelect.jsx
+++ src/common/form/LabelAndSelect.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Select from 'react-select';
 import { connect } from 'react-redux'
 import _ from 'lodash'
-
 import Grid from '../layout/grid'
 
 class LabelAndSelect extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -35,18 +36,36 @@ class LabelAndSelect extends Component {
             { insert: true, update: true, delete: true },
             { read: true, insert: true, update: true, delete: true }
         ]
-
         if (permission < 0 || permission > 15) return false
-
         if (Array.isArray(method)) {
             let result = false
             method.forEach((val) => {
                 result = !result ? permissionToMethods[permission][val] || false : true
             })
-
             return result
         } else {
             return permissionToMethods[permission][method] || false
+        }
+    }
+
+    handleSelectChange = (selectedOptions, event) => {
+        const change = this.props.input.onChange || this.props.onChange;
+
+        console.log(this.props);
+        
+    
+        if (change) {
+            let e = {
+                target: {
+                    id: this.props.input.id || this.props.id || null,
+                    name: event.name,
+                    value: selectedOptions.value,
+                    type: 'select'
+                }
+            };
+    
+            console.log('Event:', e);
+            // change(e);
         }
     }
 
@@ -79,9 +98,7 @@ class LabelAndSelect extends Component {
         if (this.props.scopes[scope] && this.props.scopes[scope].rules
             && this.props.scopes[scope].rules[this.props.input.name]) {
             const splitRules = this.props.scopes[scope].rules[this.props.input.name].split('|')
-
             splitRules.forEach(rule => {
-
                 if (rule.indexOf(':') > -1) {
                     const ruleValue = rule.split(':')
                     rules[ruleValue[0]] = ruleValue[1]
@@ -90,52 +107,62 @@ class LabelAndSelect extends Component {
                 }
             })
         }
-
         return rules
-
     }
 
     render() {
-        const formName = this.props.meta ? this.props.meta.form : null
-        let scope = ''
-        let permission = 0
-        let rules = []
-        let action = null
-
+        const formName = this.props.meta ? this.props.meta.form : null;
+        const isMulti = this.props.isMulti || false;
+        let scope = '';
+        let permission = 0;
+        let rules = [];
+        let action = null;
         if (formName) {
-            scope = _.findKey(this.props.scopes, ['entity', _.upperFirst(formName.split('Form')[0])])
-            permission = this.props.scopes[scope] ? this.props.scopes[scope].actions[this.props.input.name] || 0 : 0
-            rules = this.getValidation(scope)
-            action = this.props.forms[formName] && this.props.forms[formName].values && this.props.forms[formName].values.id ? 'update' : 'insert'
+            scope = _.findKey(this.props.scopes, ['entity', _.upperFirst(formName.split('Form')[0])]);
+            permission = this.props.scopes[scope] ? this.props.scopes[scope].actions[this.props.input.name] || 0 : 0;
+            rules = this.getValidation(scope);
+            action = this.props.forms[formName] && this.props.forms[formName].values && this.props.forms[formName].values.id ? 'update' : 'insert';
         }
-
+        const selectedValues = Array.isArray(this.props.input.value) ? this.props.input.value : [this.props.input.value];
+        const selectedOptions = this.props.options.filter(option =>
+            Array.isArray(this.props.input.value)
+                ? this.props.input.value.includes(option.id || option.value)
+                : this.props.input.value === (option.id || option.value)
+        ).map(option => ({
+            value: option.id || option.value,
+            label: option[this.props.textAttr] || option.name || option.noun || option.title || option.description || option.id
+        }));
+        const selectStyles = isMulti ? {} : { menu: base => ({ ...base, zIndex: 9999 }) };
+        const selectClassName = `basic-single-select ${this.state.error.flag === true ? `is-invalid` : ``}`;
         return (
             <>
-                {(this.props.readOnly === false || this.hasPermission(permission, 'read') || this.props.forceToShow) && <Grid cols={this.props.cols} style={this.props.style || {}} className={this.props.className || {}} >
+                {(this.props.readOnly === false || this.hasPermission(permission, 'read') || this.props.forceToShow) && <Grid cols={this.props.cols} style={{ marginBottom: '0', ...this.props.style }} className={this.props.className || {}}>
                     <div className='form-group'>
-                        <label htmlFor={this.props.name} style={{ fontWeight: '600' }}>{this.props.label}</label>
-                        <select name={this.props.name} {...this.props.input}
-                            disabled={this.props.readOnly !== false ? this.props.readOnly || !this.hasPermission(permission, action) : false}
-                            className={`form-control mb-3 ${this.state.error.flag === true ? `is-invalid` : ``}`}
-                        // required={rules['required'] || false} 
-                        >
-                            <option value="">{this.props.placeholder}</option>
-                            {this.props.options && this.props.options[0] && this.props.options.map(
-                                e => (((e && e.id > -1) || (e && e.value)) ? (<option key={e.id || e.value} value={e.id || e.value}>
-                                    {(this.props.callback ? this.props.callback(e) : null) || e[this.props.textAttr] || e.name || e.noun || e.title || e.description || e.id}
-                                </option>) : <></>)
-                            )}
-                        </select>
+                        <label htmlFor={this.props.name}>{this.props.label}</label>
+                        <Select
+                            isMulti={isMulti}
+                            name={this.props.name || this.props.input.name || null}
+                            options={this.props.options.map(option => ({
+                                value: option.id || option.value,
+                                label: option[this.props.textAttr] || option.name || option.noun || option.title || option.description || option.id
+                            }))}
+                            id={this.props.input.id || this.props.id || null}
+                            className={selectClassName}
+                            classNamePrefix="select"
+                            styles={selectStyles}
+                            // required={rules['required'] || this.props.required || false}
+                            onChange={this.handleSelectChange}
+                            value={isMulti ? selectedOptions : selectedOptions[0]} // Ajuste aqui para suportar seleção única
+                            readOnly={this.props.input.readOnly || this.props.readOnly || false}
+                            onBlur={this.props.onBlur || null}
+                            // this.props.input.onBlur vem do reduxForm e é incompatível com o react-select dessa forma.
+                            // Ele buga o react-select e limpa o valor selecionado quando é acionado
+                            onDragStart={this.props.onDragStart || this.props.input.onDragStart || null}
+                            onDrop={this.props.onDrop || this.props.input.onDrop || null}
+                            onFocus={this.props.onFocus || this.props.input.onFocus || null}
+                        />
                         <div className="invalid-feedback">
-                            {
-                                this.state.error.flag === true ?
-                                    (
-                                        this.props.input.name.includes('_') ?
-                                            this.state.error.message.replace(new RegExp(this.props.input.name.replace(/_/g, ' '), 'i'), this.props.label) :
-                                            this.state.error.message.replace(this.props.input.name, this.props.label)
-                                    ) :
-                                    "Valor inválido informado"
-                            }
+                            {this.state.error.flag && (this.state.error.message.replace(this.props.input.name, this.props.label))}
                         </div>
                     </div>
                 </Grid>}
@@ -148,4 +175,5 @@ const mapStateToProps = state => ({
     scopes: state.auth.profile.scopes,
     forms: state.form
 })
+
 export default connect(mapStateToProps, null)(LabelAndSelect)
