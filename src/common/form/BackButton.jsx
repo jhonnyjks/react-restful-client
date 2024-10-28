fec18a8a29ccf47b665f0dee2af2e17b5dd2639b
matheus1002 <matheus10carneiro@gmail.com>
2024-10-28 14:31:55 -0300

    feat: melhoria back btn

--- src/common/form/BackButton.jsx
+++ src/common/form/BackButton.jsx
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 
 export default class BackButton extends Component {
   static contextTypes = {
-    router: () => null, 
+    router: () => null,
   }
 
   render() {
-   
     const buttonStyle = {
       width: '110px',
       height: '48px',
@@ -14,12 +13,12 @@ export default class BackButton extends Component {
       borderRadius: '4px',
       borderWidth: '1px',
       borderColor: '#6C757D',
-      borderStyle: 'solid', 
-      opacity: 1, 
+      borderStyle: 'solid',
+      opacity: 1,
       display: 'inline-block',
-      whiteSpace: 'nowrap', 
-      overflow: 'hidden', 
-      textOverflow: 'ellipsis', 
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+      textOverflow: 'ellipsis',
       verticalAlign: 'middle',
     };
 
@@ -28,19 +27,25 @@ export default class BackButton extends Component {
       fontWeight: 700,
       fontSize: '16px',
       lineHeight: '24px',
-      color: '#343A40', 
+      color: '#343A40',
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
       height: '100%',
     };
 
+    // Se o onClick for false, não faz nada, senão usa o padrão do history.goBack()
+    const handleClick = this.props.onClick === false
+      ? () => { } // Função vazia, não faz nada
+      : this.props.onClick || this.context.router.history.goBack; // Se onClick for passado, usa, senão usa o padrão
+
     return (
       <button
-        type='button' 
+        type='button'
         className={`btn btn-default ${this.props.className}`}
-        style={buttonStyle} 
-        onClick={this.context.router.history.goBack}
+        style={buttonStyle}
+        onClick={handleClick} // O click agora é condicional
+        data-dismiss={this.props.dataDismiss || undefined}
       >
         <span style={textStyle}>{this.props.label}</span>
       </button>
