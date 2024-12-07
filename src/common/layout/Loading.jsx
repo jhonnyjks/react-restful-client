3839d3aa3b48a0beb91dbe12eac68b57f395270b
José Ramos Carvalho Neto <jose.carvalho@techne.com.br>
2024-12-07 14:18:43 -0300

    Loading com cor solida para esconder o modal de perfil e login

--- src/common/layout/Loading.jsx
+++ src/common/layout/Loading.jsx
@@ -7,8 +7,8 @@ class Loading extends Component {
 
     render() {
         return (
-            <div className="overlay-wrapper">
-                <div className="overlay">
+            <div className="overlay-wrapper" >
+                <div className="overlay" style={{backgroundColor:'white'}}>
                     <div className="spinner-grow text-success" style={{width: "3rem", height: "3rem"}} role="status">
                         <span className="sr-only">Carregando...</span>
                     </div>
